277db271f740c9899ab03157fb7372ac0aebcb59
김도영 <95doyoung@gmail.com>
2021-10-05 14:22:50 +0900

    feat(BaseBallGameView): 플레이어의 스윙 결과를 출력하는 View 클래스 생성
    - printSwingResult : 스윙 결과의 데이터를 이용하여 메시지를 만들어 콘솔에 출력하는 메소드 추가

--- src/main/java/baseball/view/BaseBallGameView.java
+++ src/main/java/baseball/view/BaseBallGameView.java
@@ -5,16 +5,34 @@ import baseball.model.SwingResult;
 public class BaseBallGameView {
 
     /**
-     * 스윙 결과 메시지를 콘솔에 출력합니다.
-     * */
+     * TODO 스윙 결과 메시지를 콘솔에 출력합니다.
+     */
     public static void printSwingResult(final SwingResult swingResult) {
+        final StringBuilder resultMessage = new StringBuilder();
 
+        final int strikeCount = swingResult.getStrikeCount();
+        final int ballCount = swingResult.getBallCount();
+
+        if (ballCount == 0 && strikeCount == 0) {
+            System.out.println("낫싱");
+            return;
+        }
+
+        if (strikeCount > 0) {
+            resultMessage.append(strikeCount).append("스트라이크 ");
+        }
+
+        if (ballCount > 0) {
+            resultMessage.append(ballCount).append("볼");
+        }
+
+        System.out.println(resultMessage);
     }
 
     /**
-     * 게임의 끝 인사를 콘솔에 출력합니다.
-     * */
+     * TODO 게임의 끝 인사를 콘솔에 출력합니다.
+     */
     public static void printEndMessage() {
-
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 끝");
     }
 }

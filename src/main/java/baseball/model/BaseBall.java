8b4e5e3d0bfbf02ebdef2520d7998493a945aa49
김도영 <95doyoung@gmail.com>
2021-10-06 20:59:04 +0900

    docs(BaseBall): 좀더 명확한 의미 전달하기 위해 주석 수정
    - indexOf
    - validateFormation
    - validateDuplicatedSize
    - contains
    - containsRecursive

--- src/main/java/baseball/model/BaseBall.java
+++ src/main/java/baseball/model/BaseBall.java
@@ -45,7 +45,7 @@ public class BaseBall {
 	}
 
 	/**
-	 * 투구번호 배열에서 <code>number</code>가 있는지 확인합니다.
+	 * 투구번호 배열에서 <code>number</code>가 어느 인덱스에 있는지 확인합니다.
 	 *
 	 * @param number 확인하고자 하는 숫자
 	 * @return {@link BaseBall#containsRecursive(int[], int, int, int)}
@@ -85,7 +85,7 @@ public class BaseBall {
 	}
 
 	/**
-	 * 1~9 로 구성된 서로 다른 임의의 숫자 3개가 있는지 확인
+	 * 1~9 로 구성된 숫자 3개가 있는지 확인
 	 *
 	 * @param textBaseBall 문자열로 이루어진 투구번호
 	 * @throws IllegalBaseBallArgumentException 문자열이 1~9까지의 임의의수 3개가 아닐경우 발생합니다.
@@ -99,10 +99,10 @@ public class BaseBall {
 	}
 
 	/**
-	 * 1~9 로 구성된 서로 다른 임의의 숫자 3개가 있는지 확인
+	 * 중복된 숫자가 있는지 중복을 제거 전후의 크기를 비교합니다.
 	 *
 	 * @param textBaseBall 문자열로 이루어진 투구번호
-	 * @throws IllegalBaseBallArgumentException 문자열이 1~9까지의 임의의수 3개가 아닐경우 발생합니다.
+	 * @throws IllegalBaseBallArgumentException 중복된 숫자가 있을 경우 발생합니다.
 	 */
 	private static void validateDuplicatedSize(final String textBaseBall) throws IllegalBaseBallArgumentException {
 		final Set<Integer> numberSet = new HashSet<>();
@@ -157,15 +157,16 @@ public class BaseBall {
 	 * @param baseBall 투구번호가 들어가 있는 int 배열
 	 * @param number   확인하고자 하는 숫자
 	 * @param range    확인하고자 하는 배열의 범위
-	 * @return {Boolean} 있으면 True 없으면 false
+	 * @return 포함 하고 있으면 True 없으면 false
 	 */
 	private static boolean contains(final int[] baseBall, final int number, final int range) {
 		return containsRecursive(baseBall, number, 0, range) > -1;
 	}
 
 	/**
-	 * <code>number</code>의 값을 찾을 때 까지 <code>start</code>의 값을 +1 하여 재귀적으로 함수를 실행합니다.
-	 * <code>start</code> 의 값이 <code>end</code> 의 값보다 큰 경우 <code>baseBall</code> 배열의 존재하지 않는다는 것으로 간주합니다.
+	 * <code>number</code>의 값을 찾을 때 까지 <code>start</code>의 값을 +1 하여 재귀적으로 실행합니다.
+	 * <code>start</code> 의 값이 <code>end</code> 의 값보다 큰 경우 <code>baseBall</code> 배열의 존재하지
+	 * 않는다는 것으로 간주하여 -1 을 반환합니다.
 	 *
 	 * @param baseBall 투구번호가 들어가 있는 int 배열
 	 * @param number   확인하고자 하는 숫자
